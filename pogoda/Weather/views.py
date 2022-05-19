a8a6e686b15636d7b7fb963c4b34c958ab975fcf
Hello <ksnosha@mail.ru>
2022-05-19 14:55:12 +0300

    url

--- pogoda/Weather/views.py
+++ pogoda/Weather/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+import requests
+
 def index(request):
+    appid = '1a516c056bcd9d98df7e10ca6b794c70'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=" + "appid"
+
+    city = 'London'
+
     return render(request, 'weather/index.html')
 
