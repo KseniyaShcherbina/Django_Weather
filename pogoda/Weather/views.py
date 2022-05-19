398cbf26b941046dfb0222ba28054c5443d539ff
Hello <ksnosha@mail.ru>
2022-05-19 15:07:26 +0300

    minsk

--- pogoda/Weather/views.py
+++ pogoda/Weather/views.py
@@ -3,9 +3,11 @@ import requests
 
 def index(request):
     appid = '1a516c056bcd9d98df7e10ca6b794c70'
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=" + "appid"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
 
-    city = 'London'
+    city = 'Minsk'
+    res = requests.get(url.format(city))
+    print(res.text)
 
     return render(request, 'weather/index.html')
 
