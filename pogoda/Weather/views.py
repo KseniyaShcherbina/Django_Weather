d56a763c83ff6c397e8983228dbc0745b5607edb
Hello <ksnosha@mail.ru>
2022-05-23 13:27:46 +0300

    form

--- pogoda/Weather/views.py
+++ pogoda/Weather/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = '1a516c056bcd9d98df7e10ca6b794c70'
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm
 
     cities = City.objects.all()
 
@@ -19,7 +26,7 @@ def index(request):
         }
         all_cities.append(city_info)
 
-    context = {'all_info':all_cities}
+    context = {'all_info':all_cities, 'form':form}
 
     return render(request, 'weather/index.html', context)
 
