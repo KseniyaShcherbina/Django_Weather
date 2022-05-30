eb49f4c000223eec2ac07245dc195bc72c60007e
Hello <ksnosha@mail.ru>
2022-05-30 10:47:56 +0300

    UserView

--- pogoda/users/views.py
+++ pogoda/users/views.py
@@ -40,3 +40,19 @@ class LoginView(APIView):
         return response
 
 
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Не прошедший проверку подлинности')
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Не прошедший проверку подлинности')
+
+        user = User.objects.filter(id=payload['id']).first()
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
