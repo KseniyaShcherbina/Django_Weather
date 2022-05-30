a2d4a425390f94cf5ee0731bc4afe9951b8e3f7a
Hello <ksnosha@mail.ru>
2022-05-30 10:29:11 +0300

    cookie

--- pogoda/users/views.py
+++ pogoda/users/views.py
@@ -31,7 +31,12 @@ class LoginView(APIView):
 
         token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
 
-        return Response({
-          'jwt': token
-        })
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data ={
+            'jwt': token
+        }
+
+        return response
+
 
